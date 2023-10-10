784b8bf66d17018337608153d5109abbb11ccf53
kanghyeji <kanghj9408@gmail.com>
2023-10-10 18:15:01 +0900

    refactor: lint-staged config 에서 타입체크 값을 고차함수로 개선

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,17 @@
+const MODULES = [
+  { name: 'random-menu', parent: 'apps' },
+  { name: 'common-utils', parent: 'packages' },
+  { name: 'common-ui', parent: 'packages' },
+]
+const typeCheckConfigs = MODULES.reduce(
+  (prev, { name, parent }) => ({
+    ...prev,
+    [`./${parent}/${name}/**/*.{ts,tsx}`]: () =>
+      `pnpm run --filter ${name} type-check`,
+  }),
+  {},
+)
+
 export default {
   // linting
   '*.{ts,tsx}': 'eslint --cache --max-warnings=0',
@@ -6,10 +20,5 @@ export default {
   '*.{ts,tsx,css,md}': 'prettier --write',
 
   // type check
-  './apps/random-menu/**/*.{ts,tsx}': () =>
-    'pnpm run --filter random-menu type-check',
-  './packages/common-utils/**/*.{ts,tsx}': () =>
-    'pnpm run --filter common-utils type-check',
-  './packages/common-ui/**/*.{ts,tsx}': () =>
-    'pnpm run --filter common-ui type-check',
+  ...typeCheckConfigs,
 }
