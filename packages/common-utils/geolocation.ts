afa422d2b8797fe60e80d8c80b282442aa4cdb20
kanghyeji <kanghj9408@gmail.com>
2023-10-13 16:12:23 +0900

    refactor: getCurrentPosition util Promise 를 리턴하도록 수정

--- packages/common-utils/geolocation.ts
+++ packages/common-utils/geolocation.ts
@@ -1,10 +1,26 @@
-export const getCurrentPosition = (): GeolocationPosition | undefined => {
+import { errorHandler } from './error'
+
+export const getCurrentPosition = async (): Promise<
+  GeolocationPosition | undefined
+> => {
   const { geolocation } = navigator || {}
   if (!geolocation) {
     return
   }
 
-  geolocation.getCurrentPosition((position) => {
-    return position
+  return new Promise<GeolocationPosition>((resolve) => {
+    try {
+      geolocation.getCurrentPosition((position) => {
+        resolve(position)
+      })
+    } catch (err) {
+      errorHandler(err, '현재 위치를 조회하는 중')
+    }
   })
+}
+
+export const getLatLng = async (): Promise<{ lat: number; lng: number }> => {
+  const currPosition = await getCurrentPosition()
+  const { coords } = currPosition || {}
+  return { lat: coords?.latitude || 0, lng: coords?.longitude || 0 }
 }
