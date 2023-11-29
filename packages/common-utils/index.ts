b41cfd40ff55517ec353a0c5f18a976d53c27a93
kanghyeji <kanghj9408@gmail.com>
2023-11-29 21:16:18 +0900

    chore: TS compilerOption값 셋팅에 따라 글로벌 타입정의가 무시되어, DOM타입을 선언해줄 수 있는 지시어 추가

--- packages/common-utils/index.ts
+++ packages/common-utils/index.ts
@@ -1,2 +1,9 @@
+/**
+ * https://bun.sh/docs/typescript
+ * tsconfig에 bun-type을 위해 types 옵션을 지정하게 되면 글로벌 타입 정의를 무시하게 된다.
+ * 이에 따라 DOM 글로벌 타입을 추가하기 위해 아래 triple-slash 지시어를 추가해줘야 한다.
+ * */
+/// <reference lib="dom" />
+/// reference lib="dom.iterable" />
 export * from './geolocation'
 export * from './generator'
